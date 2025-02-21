a5954d63c7ff582b6f0c717668e95493dc4f60ad
Angel Luna Lugo <131845927+AngeL1una@users.noreply.github.com>
2025-02-20 19:28:54 -0600

    Modifiqué el archivo page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   
       <main className="flex flex-col items-center p-24 text-white">
         <span className="text-5xl ">
-          Hola mundo
+          Hola mundo soy angel luna
         </span>
         <Link href={'/about'}>About Page</Link>
       </main>
